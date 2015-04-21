edece26593a78b8efce470f3ccc73632c1354728
Oliver Davies <oliver@oliverdavies.co.uk>
2015-04-21 13:42:03 +0100

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,39 @@
+define drupal_site ($enable) {
+
+  include apache
+
+  if (defined("apache::vhost")) {
+    # Set some default values for vhosts.
+    Apache::Vhost {
+      port     => '80',
+      override => 'All',
+      notify   => Service['httpd'],
+    }
+
+    apache::vhost { "${name}":
+      docroot => "/var/www/html/${name}",
+    }
+  }
+
+  if (defined(Class["mysql::server"])) {
+    mysql_database { "${name}-db":
+      require => Class['mysql::server'],
+      name    => $name,
+      ensure  => present,
+    }
+
+    mysql_user { "${name}@localhost":
+      require       => Class['mysql::server'],
+      name          => "${name}@localhost",
+      password_hash => mysql_password($name)
+    }
+
+    mysql_grant { "${name}@localhost/${name}.*":
+      require    => Class['mysql::server'],
+      user       => "${name}@localhost",
+      table      => "${name}.*",
+      privileges => ["ALL"],
+      ensure     => "present"
+    }
+  }
+}
